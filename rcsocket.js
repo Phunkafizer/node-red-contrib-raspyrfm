51bde901990307f31e777fb887135012aee9a3a5
S. Seegel <post@seegel-systeme.de>
2020-11-19 09:10:08 +0100

    add support for it32 and pdm32

--- rcsocket.js
+++ rcsocket.js
@@ -3,7 +3,7 @@ var net = require('net');
 module.exports = function(RED) {
     function RCSocketNode(n) {  
         RED.nodes.createNode(this, n);
-
+        this.log("rcsocket create");
         this.server = RED.nodes.getNode(n.server);
         var node = this;
 
@@ -18,10 +18,24 @@ module.exports = function(RED) {
 
             switch (n.datatype) {
                 case 'IT':
-                    payload.protocol = "ittristate"
+                    payload.protocol = "ittristate";
                     payload.house = n.house;
                     payload.unit = parseInt(n.unit);
                     payload.state = msg.payload == "on" ? 1 : 0;
+                    node.server.socket.write(JSON.stringify(payload));
+                    break;
+
+                case 'IT32':
+                    payload.protocol = "it32";
+                    payload.id = parseInt(n.rcid);
+                    payload.unit = parseInt(n.unit);
+                    payload.state = msg.payload == "on" ? 1 : 0;
+                    node.server.socket.write(JSON.stringify(payload));
+                    break;
+
+                case 'PDM32':
+                    payload.protocol = "pdm32";
+                    payload.code = msg.payload == "on" ? n.codeon : n.codeoff;
                     node.server.socket.write(JSON.stringify(payload));
                     break;
             }
